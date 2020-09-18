98a482ee92ff8a22f24fc75ab9de5e88b02c1d84
jmonlong <jmonlong@ucsc.edu>
2020-09-17 22:25:20 -0700

    start making overlapping cleaner and more flexible, aka not only coverage style

--- R/annotateOl.R
+++ R/annotateOl.R
@@ -1,6 +1,7 @@
 ##' @title Annotate SVs from overlap
 ##' @param ol.l output of an overlap function (olInsertions or olRanges).
 ##' @param min.qual the minimum quality considered for the calls.
+##' @param min.cov the minimum coverage to be considered a match. Default is 0.5
 ##' @param method the method to annotate the overlap. Either 'coverage' (default) for the
 ##' cumulative coverage (e.g. to deal with fragmented calls); or 'bipartite' for a 1-to-1
 ##' matching of variants in the calls and truth sets.
@@ -9,15 +10,16 @@
 ##' @importFrom magrittr %>%
 ##' @importFrom rlang .data
 ##' @keywords internal
-annotateOl <- function(ol.l, min.qual=0, method=c('coverage', 'bipartite')){
+annotateOl <- function(ol.l, min.qual=0, min.cov=.5, method=c('coverage', 'bipartite')){
   hq.idx = which(ol.l$calls$qual >= min.qual)
   if(length(ol.l$truth)>0){
-    ol.l$truth$cov = 0
+    ol.l$truth$ol = FALSE
   }
   if(length(ol.l$calls)>0){
-    ol.l$calls$cov = 0
+    ol.l$calls$ol = FALSE
   }
   if('ol' %in% names(ol.l)){
+    ## this is for insertions where we have a list of pairs of insertions that cluster together
     ## filter based on quality
     ol.l$ol = ol.l$ol[which(ol.l$ol$call.idx %in% hq.idx),]
     if(length(ol.l$ol)>0 && nrow(ol.l$ol)>0){
@@ -26,12 +28,16 @@ annotateOl <- function(ol.l, min.qual=0, method=c('coverage', 'bipartite')){
         ins.truth.cov = ol.l$ol %>%
           dplyr::group_by(.data$truth.idx) %>%
           dplyr::summarize(cov=sum(.data$truth.cov))
-        ol.l$truth$cov[ins.truth.cov$truth.idx] = ins.truth.cov$cov
+        cov.temp = rep(0, length(ol.l$truth))
+        cov.temp[ins.truth.cov$truth.idx] = ins.truth.cov$cov
+        ol.l$truth$ol = cov.temp / ol.l$truth$size >= min.cov
         ## Coverage on call set
         ins.call.cov = ol.l$ol %>%
           dplyr::group_by(.data$call.idx) %>%
           dplyr::summarize(cov=sum(.data$call.cov))
-        ol.l$calls$cov[ins.call.cov$call.idx] = ins.call.cov$cov
+        cov.temp = rep(0, length(ol.l$calls))
+        cov.temp[ins.call.cov$call.idx] = ins.call.cov$cov
+        ol.l$calls$ol = cov.temp / ol.l$calls$size >= min.cov
       } else if(method[1] == 'bipartite'){
         ## label both sets
         ol.l$ol$call.lab = paste0('call_', ol.l$ol$call.idx)
@@ -60,16 +66,19 @@ annotateOl <- function(ol.l, min.qual=0, method=c('coverage', 'bipartite')){
         bpm = bpm[which(!is.na(bpm))]
         ## matched variants: variants in 2-variants comp or bipartite matched
         matched.labs = c(comp2.v, names(bpm), as.character(bpm))
-        ## translate into "coverage" for the rest of the pipeline
+        ## annotate the 'ol' column in each SV set
         truth.idx.m = ol.l$ol$truth.idx[which(ol.l$ol$truth.lab %in% matched.labs)]
-        ol.l$truth$cov[truth.idx.m] = ol.l$truth$size[truth.idx.m]
+        ol.l$truth$ol[truth.idx.m] = TRUE
         call.idx.m = ol.l$ol$call.idx[which(ol.l$ol$call.lab %in% matched.labs)]
-        ol.l$calls$cov[call.idx.m] = ol.l$calls$size[call.idx.m]
+        ol.l$calls$ol[call.idx.m] = TRUE
       } else {
         stop('"method=" must be one of "coverage" or "bipartite".')
       }
+    } else {
+      ol.o$ol = data.frame()
     }
   } else if('rol.gr' %in% names(ol.l)){
+    ## this is when comparing ranges, e.g. deletions or inversions
     rol.gr = ol.l$rol.gr[which(ol.l$rol.gr$call.idx %in% hq.idx)]
     if(length(rol.gr)>0){
       if(method[1]=='coverage'){
@@ -77,12 +86,16 @@ annotateOl <- function(ol.l, min.qual=0, method=c('coverage', 'bipartite')){
         gr.l = GenomicRanges::GRangesList(GenomicRanges::split(rol.gr, rol.gr$truth.idx))
         gr.l = GenomicRanges::reduce(gr.l)
         cov.l = lapply(GenomicRanges::width(gr.l), sum)
-        ol.l$truth$cov[as.numeric(names(cov.l))] = unlist(cov.l)
+        cov.temp = rep(0, length(ol.l$truth))
+        cov.temp[as.numeric(names(cov.l))] = unlist(cov.l)
+        ol.l$truth$ol = cov.temp /ol.l$truth$size >= min.cov
         ## Overlap coverge on the call set
         gr.l = GenomicRanges::GRangesList(GenomicRanges::split(rol.gr, rol.gr$call.idx))
         gr.l = GenomicRanges::reduce(gr.l)
         cov.l = lapply(GenomicRanges::width(gr.l), sum)
-        ol.l$calls$cov[as.numeric(names(cov.l))] = unlist(cov.l)
+        cov.temp = rep(0, length(ol.l$calls))
+        cov.temp[as.numeric(names(cov.l))] = unlist(cov.l)
+        ol.l$calls$ol = cov.temp /ol.l$calls$size >= min.cov
       } else if(method[1] == 'bipartite'){
         ## label both sets
         rol.gr$call.lab = paste0('call_', rol.gr$call.idx)
@@ -112,11 +125,11 @@ annotateOl <- function(ol.l, min.qual=0, method=c('coverage', 'bipartite')){
         bpm = bpm[which(!is.na(bpm))]
         ## matched variants: variants in 2-variants comp or bipartite matched
         matched.labs = c(comp2.v, names(bpm), as.character(bpm))
-        ## translate into "coverage" for the rest of the pipeline
+        ## annotate the 'ol' column in each SV set
         truth.idx.m = rol.gr$truth.idx[which(rol.gr$truth.lab %in% matched.labs)]
-        ol.l$truth$cov[truth.idx.m] = ol.l$truth$size[truth.idx.m]
+        ol.l$truth$ol[truth.idx.m] = TRUE
         call.idx.m = rol.gr$call.idx[which(rol.gr$call.lab %in% matched.labs)]
-        ol.l$calls$cov[call.idx.m] = ol.l$calls$size[call.idx.m]
+        ol.l$calls$ol[call.idx.m] = TRUE
       } else {
         stop('"method=" must be one of "coverage" or "bipartite".')
       }
--- R/evalOl.R
+++ R/evalOl.R
@@ -1,29 +1,26 @@
 ##' @title Compute FP, FN, TP
 ##' @param ol.l list returned by annotation functions like \code{annotateOl}
-##' @param min.cov the minimum coverage to be considered a match. Default is 0.5
 ##' @return a data.frame with FP, FN and TP for each SV type.
 ##' @author Jean Monlong
 ##' @importFrom magrittr %>%
 ##' @importFrom rlang .data
 ##' @keywords internal
-evalOl <- function(ol.l, min.cov=.8){
+evalOl <- function(ol.l){
   if(length(ol.l$calls)==0 | length(ol.l$truth)==0){
     eval.df = data.frame(type=c('Total', 'DEL', 'INS', 'INV'), stringsAsFactors=FALSE)
     eval.df$F1 = eval.df$recall = eval.df$precision = eval.df$FN = eval.df$FP = eval.df$TP.baseline = eval.df$TP = NA
     regs = list()
   } else {
     ## Compute TP and FN from the truth set
-    eval.df = GenomicRanges::mcols(ol.l$truth)[,c('type','cov','size')] %>%
+    eval.df = GenomicRanges::mcols(ol.l$truth)[,c('type','ol')] %>%
       as.data.frame %>% 
       dplyr::group_by(.data$type) %>%
-      dplyr::summarize(FN=sum(.data$cov / .data$size < min.cov),
-                       TP.baseline=sum(.data$cov / .data$size >= min.cov))
+      dplyr::summarize(FN=sum(!.data$ol), TP.baseline=sum(.data$ol))
     ## Compute TP and FP from the call set and merge adding NAs if necessary
-    eval.df = GenomicRanges::mcols(ol.l$calls)[,c('type','cov','size')] %>%
+    eval.df = GenomicRanges::mcols(ol.l$calls)[,c('type','ol')] %>%
       as.data.frame %>% 
       dplyr::group_by(.data$type) %>%
-      dplyr::summarize(FP=sum(.data$cov / .data$size < min.cov),
-                       TP=sum(.data$cov / .data$size >= min.cov)) %>%
+      dplyr::summarize(FP=sum(!.data$ol), TP=sum(.data$ol)) %>%
       merge(eval.df, all=TRUE)
     ## Convert NAs to 0
     eval.df = as.data.frame(lapply(eval.df, function(x) ifelse(is.na(x), 0, x)))
@@ -44,19 +41,19 @@ evalOl <- function(ol.l, min.cov=.8){
       regs = list()
       ## Truth
       gr = ol.l$truth[which(ol.l$truth$type == svtype)]
-      grr = gr[which(gr$cov / gr$size < min.cov)]
+      grr = gr[which(!gr$ol)]
       grr$alt = NULL
       regs$FN = grr
-      grr = gr[which(gr$cov / gr$size >= min.cov)]
+      grr = gr[which(gr$ol)]
       grr$alt = NULL
       regs$TP.baseline = grr
       ## Calls
       gr = ol.l$calls[which(ol.l$calls$type == svtype)]
-      grr = gr[which(gr$cov / gr$size < min.cov)]
+      grr = gr[which(!gr$ol)]
       grr$alt = NULL
       regs$FP = grr
       gr$alt = NULL
-      grr = gr[which(gr$cov / gr$size >= min.cov)]
+      grr = gr[which(gr$ol)]
       regs$TP = grr
       regs
     })
